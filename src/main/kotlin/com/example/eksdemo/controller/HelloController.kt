fa9e81e4ebef8c2c1b71602ae827fe9ba9778ab9
asuraiv <asuraiv7@gmail.com>
2021-02-01 23:13:07 +0900

    hello 메시지 수정

--- src/main/kotlin/com/example/eksdemo/controller/HelloController.kt
+++ src/main/kotlin/com/example/eksdemo/controller/HelloController.kt
@@ -7,14 +7,12 @@ import java.net.InetAddress
 
 @RestController
 class HelloController(
-    @Value("\${spring.profiles.active}")
-    val activeProfile: String,
     @Value("\${service.name}")
     val serviceName: String?
 ) {
 
     @GetMapping("/hello")
     fun hello(): String {
-        return "[${activeProfile.toUpperCase()}][$serviceName][Version 0.0.1] Receive message from ${InetAddress.getLocalHost().hostName}\n"
+        return "[$serviceName][Version 0.0.1] Receive message from ${InetAddress.getLocalHost().hostName}\n"
     }
 }
