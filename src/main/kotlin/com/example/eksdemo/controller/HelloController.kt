22320eac637b9ca0b31309ca1e6cbf9b9fa797bb
asuraiv <asuraiv7@gmail.com>
2021-02-01 21:31:25 +0900

    hello 메시지 변경

--- src/main/kotlin/com/example/eksdemo/controller/HelloController.kt
+++ src/main/kotlin/com/example/eksdemo/controller/HelloController.kt
@@ -1,14 +1,20 @@
 package com.example.eksdemo.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.InetAddress
 
 @RestController
-class HelloController {
+class HelloController(
+    @Value("\${spring.profiles.active}")
+    val activeProfile: String,
+    @Value("\${service.name}")
+    val serviceName: String
+) {
 
     @GetMapping("/hello")
     fun hello(): String {
-        return "[Version 0.0.6] Receive message from ${InetAddress.getLocalHost().hostName}\n"
+        return "[${activeProfile.toUpperCase()}][$serviceName][Version 0.0.1] Receive message from ${InetAddress.getLocalHost().hostName}\n"
     }
 }
